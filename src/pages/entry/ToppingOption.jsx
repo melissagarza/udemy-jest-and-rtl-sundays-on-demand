665d6f66ece26c6840ad1b485b4571768517f4f9
Melissa Garza <melissa.garza@utexas.edu>
2021-05-11 11:06:02 -0500

    Test and code topping option selection and subtotal

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-    <img
-      src={`http://localhost:3030/${imagePath}`}
-      alt={`${name} topping`}
-      style={{ width: '75%' }}
-    />
-  </Col>
+    <>
+      <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <img
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+        style={{ width: '75%' }}
+      />
+    </Col>
+    <Form.Group controlId={`${name}-topping-checkbox`}>
+      <Form.Check
+        type="checkbox"
+        label={name}
+        onChange={e => { updateItemCount(name, e.target.checked ? 1 : 0); }}
+      />
+    </Form.Group>
+    </>
   );
 };
 
--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
 test('update scoop subtotal when scoops change', async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider});
+  render(<Options optionType="scoops" />);
 
   const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
 
@@ -21,4 +20,30 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
+
+test('update topping subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', { name: 'Cherries' });
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', { name: 'Hot fudge' });
+
+  expect(cherriesCheckbox).not.toBeChecked();
+  expect(hotFudgeCheckbox).not.toBeChecked();
+
+  userEvent.click(cherriesCheckbox);
+  expect(cherriesCheckbox).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  userEvent.click(hotFudgeCheckbox);
+  expect(hotFudgeCheckbox).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  userEvent.click(hotFudgeCheckbox);
+  expect(hotFudgeCheckbox).not.toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
 });
